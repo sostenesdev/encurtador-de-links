c1ab2cc94a0b6e11c336268c03ed9381143ec9e2
Sostenes Junior <sostenes.junior@serra.es.gov.br>
2025-02-25 09:50:03 -0300

    correção na utilização do token de acesso

--- resources/js/axiosConfig/index.js
+++ resources/js/axiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import config from '../config';
 
 // Supondo que você tenha o token armazenado em algum lugar, por exemplo, no localStorage
-const token = localStorage.getItem('token');
+// const token = localStorage.getItem('token');
 
 // Configurar o cabeçalho Authorization com o Bearer Token
 const api = axios.create({
